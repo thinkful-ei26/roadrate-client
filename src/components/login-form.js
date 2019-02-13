bcdc8fb2036a9e930ef685d0bac579f38f1ec17f
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-12 20:21:04 -0500

    redux init

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,34 +1,92 @@
-import React, { useState } from 'react'; 
+import React from 'react';
+import {Field, reduxForm, focus} from 'redux-form';
+import Input from './input';
+import {login} from '../actions/auth';
+import {required, nonEmpty} from '../validators';
 
-export function LoginForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+export class LoginForm extends React.Component {
+    onSubmit(values) {
+        return this.props.dispatch(login(values.username, values.password));
+    }
 
-  return(
-    <form className="login">
-      <label htmlFor="username">Username: </label>
-      <input
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-        placeholder="Username"
-        type="text"
-        name="username"
-        required
-      />
-      <label htmlFor="password">Password: </label>
-      <input
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        placeholder="Password"
-        type="password"
-        name="password"
-        required
-      />
-      <button type="submit">
-        Submit
-      </button>
-    </form>
-  );
+    render() {
+        let error;
+        if (this.props.error) {
+            error = (
+                <div className="form-error" aria-live="polite">
+                    {this.props.error}
+                </div>
+            );
+        }
+        return (
+            <form
+                className="login-form"
+                onSubmit={this.props.handleSubmit(values =>
+                    this.onSubmit(values)
+                )}>
+                {error}
+                <label htmlFor="username">Username</label>
+                <Field
+                    component={Input}
+                    type="text"
+                    name="username"
+                    id="username"
+                    validate={[required, nonEmpty]}
+                />
+                <label htmlFor="password">Password</label>
+                <Field
+                    component={Input}
+                    type="password"
+                    name="password"
+                    id="password"
+                    validate={[required, nonEmpty]}
+                />
+                <button disabled={this.props.pristine || this.props.submitting}>
+                    Log in
+                </button>
+            </form>
+        );
+    }
 }
 
-export default LoginForm;
+export default reduxForm({
+    form: 'login',
+    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+})(LoginForm);
+
+
+
+// import React, { useState } from 'react'; 
+
+// export function LoginForm() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   return(
+//     <form className="login">
+//       <label htmlFor="username">Username: </label>
+//       <input
+//         value={username}
+//         onChange={e => setUsername(e.target.value)}
+//         placeholder="Username"
+//         type="text"
+//         name="username"
+//         required
+//       />
+//       <label htmlFor="password">Password: </label>
+//       <input
+//         value={password}
+//         onChange={e => setPassword(e.target.value)}
+//         placeholder="Password"
+//         type="password"
+//         name="password"
+//         required
+//       />
+//       <button type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
+
+// export default LoginForm;
