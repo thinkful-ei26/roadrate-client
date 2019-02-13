49c6c18f7e01a5574d8f388f8d55ce5ef7a50042
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-13 11:22:38 -0500

    printing login userInput

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -6,8 +6,8 @@ console.log(API_BASE_URL);
 export const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const initialFormState = { id: null, name: '', username: '' }
-  const [ user, setUser ] = useState(initialFormState)
+  const [inputValue, setValue] = useState("userInput placeholder");
+  const [ user, setUser ] = useState(inputValue)
    
     // Use an async function so that we can await the fetch
     useEffect(async () => {
@@ -17,49 +17,17 @@ export const LoginForm = () => {
       );
   
       // Pull out the data as usual
-      const json = await res.json();
-  
-      console.log('JSON: ', json)
-      setUser(json);
-    }, []); // <-- we didn't pass a value. what do you think will happen?
-  
-
-  // const useFetch = url => {
-  //   const [data, setData] = useState(null);
-  //   const [loading, setLoading] = useState(true);
-  
-  //   // Similar to componentDidMount and componentDidUpdate:
-  //   useEffect(async () => {
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     const [item] = data.results;
-  //     setData(item);
-  //     setLoading(false);
-  //   }, []);
-  
-  //   return { data, loading };
-  // };
-
-  // const { data, loading } = useFetch(`${API_BASE_URL}/users`);
-
-  // console.log('data', data);
-
-  // fetchComment(commentId) {
-  //   // You can await here
-  // }
-  
-  // useEffect(() => {
-  //   fetchComment(commentId);
-  // }, [commentId]);
+      const users = await res.json();
+      console.log('USERS : ', users)
+      setUser(users);
+    }, [setUsername]); 
   
   const handleSubmit = e => {
-    const { username, password } = e.target;
     e.preventDefault(); 
-    if (!username || !username) return
+    if (!username || !username) return;
     console.log(`user info: ${username} and ${password}`)
-    //call a fn that posts the userinputs to the db
-    // call setUsername with that value
-    setUsername()
+    setUsername(username)
+    setPassword(password)
   }
 
   return(
