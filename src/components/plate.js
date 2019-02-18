07c417247ab37c0a052b647fc4e4e019eedf26f1
Jordan <jordanhaddadi@gmail.com>
2019-02-18 12:40:08 -0500

    created modal for plate reviews

--- src/components/plate.js
+++ src/components/plate.js
@@ -1,17 +1,36 @@
 import React  from 'react'; 
+import ReactModal from "react-modal";
+import { useModal }  from 'react-modal-hook';
+
+// const customStyles = {
+//   content : {
+//     top                   : '50%',
+//     left                  : '50%',
+//     right                 : 'auto',
+//     bottom                : 'auto',
+//     marginRight           : '-50%',
+//     transform             : 'translate(-50%, -50%)'
+//   }
+// };
 
 export const Plate = () => {
-  
+  const [showModal, hideModal] = useModal(() => (
+    <ReactModal isOpen>
+      <div className="plate">
+        <h4>PLATE NUMBER</h4>
+        <div className="karma-wrapper">
+          <p className="karma-score">Karma Score: #Number</p>
+        </div>
+        <p>INSERT PLATE REVIEWS COMPONENT</p>
+        <button onClick={hideModal}>Close</button>
+      </div>
+    </ReactModal>
+  ));
+
   return (
     // ==== When a User clicks on a plate link inside the review page, this will render
-    <div className="plate">
-      <h4>MY PLATE NUMBER</h4>
-      <div className="karma-wrapper">
-        <p className="karma-score">Karma Score: #Number</p>
-      </div>
-      <p>INSERT PLATE REVIEWS COMPONENT</p>
-    </div>
+    <button onClick={showModal}>Show Plate Reviews</button>
   );
-}
+};
 
 export default Plate;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import {BrowserRouter as Router} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import store from './store';
+// import {Provider} from 'react-redux';
+import { ModalProvider } from 'react-modal-hook';
+// import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ModalProvider>
       <Router>
           <App />
       </Router>
-  </Provider>,
+  </ModalProvider>,
   document.getElementById('root')
 );
