195df5e5f590f950552628e8e9974ab9ccdfa745
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-19 16:06:08 -0500

    linking plates working on client side

--- src/components/claim-plate.js
+++ src/components/claim-plate.js
@@ -10,40 +10,34 @@ export const claimPlate = (props) => {
   const [ plateState, setPlateState ] = useState('');
   const [ plates, setPlates ] = useState("");
 
-  // const handleLinkClick = e => {
-  //   e.preventDefault();
-  //   console.log('handleLinkClick fired');
-  //   return fetch(`${API_BASE_URL}/plates/${localStorage.userId}`, {
-  //     method: 'PUT',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json',
-  //       'Authorization': `Bearer ${localStorage.authToken}`
-  //     },
-  //     body: JSON.stringify({
-  //       userId: localStorage.userId,
-  //       plateNumber
-  //     })
-  //   })
-  //   .then(res => {
-  //     console.log('res inside handleLinkClick', res);
-  //     return res.json();
-  //   })
-  //   .then(data => console.log('DATA CLAIM PLATE:', data))
-  //   .catch(err => {
-  //     console.log(err)
-  //     if(err === 'TypeError: Failed to fetch'){
-  //       console.log('duplicate error')
-  //       return Promise.reject(err)
-  //     }
-  //     console.log(err)
-  //   })
-  // }
+  const handleLinkClick = e => {
+    e.preventDefault();
+    console.log('handleLinkClick fired');
+    return fetch(`${API_BASE_URL}/plates/${localStorage.userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${localStorage.authToken}`
+      },
+      body: JSON.stringify({
+        userId: localStorage.userId,
+        plateNumber
+      })
+    })
+    .then(res => {
+      console.log('res inside handleLinkClick', res);
+      return res.json();
+    })
+    .then(data => console.log('DATA CLAIM PLATE:', data))
+    .catch(err => console.log(err))
+  }
 
+  /* ========= SEARCH LICENSE PLATE TO LINK ========== */
   const handleSubmit = e => {
     e.preventDefault(); 
-    if (!plateNumber ||  plateNumber === '') return;
-    if (!plateState ||  plateState === '') return;
+    // if (!plateNumber ||  plateNumber === '') return;
+    // if (!plateState ||  plateState === '') return;
     console.log('clicked search btn', plateNumber)
     console.log(plates);
 
@@ -105,7 +99,14 @@ export const claimPlate = (props) => {
           <td>{plates.karma}</td>
           {/* need to get reviews.length of all of the reviews that have ever mentioned this license plate */}
           <td>0</td>
-          <td><button className='link-to-user-button' /* onClick={handleLinkClick()} */>Link</button></td>
+          <td>
+            <button 
+              className='link-to-user-button' 
+              onClick={(e) => handleLinkClick(e)}
+            >
+              Link
+            </button>
+          </td>
         </tr>
       </table>
     )
@@ -154,7 +155,11 @@ export const claimPlate = (props) => {
         </label>
 
         <label htmlFor='plateState'>State: 
-          <select className='browser-default' value={plateState} onChange={(e) => setPlateState(e.target.value)}>
+          <select 
+            className='browser-default' 
+            value={plateState} 
+            onChange={(e) => setPlateState(e.target.value)}
+          >
             <option value=''>Select State</option>
             <option value="AL">Alabama</option>
             <option value="AK">Alaska</option>
