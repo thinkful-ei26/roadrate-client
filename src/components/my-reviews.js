a4449186a97c11f5973f38bb92597160e4e0d7f8
randy sartor <randal.sartor@gmail.com>
2019-02-22 13:37:21 -0500

    added time properties and refactored components that list reviews

--- src/components/my-reviews.js
+++ src/components/my-reviews.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect }  from 'react';
 import {API_BASE_URL} from '../config';
 import { Icon } from 'react-materialize';
 import { Link } from 'react-router-dom';
+import Review from './Review'
 import '../styles/my-reviews.css';
 
 export const MyReviews = (props) => {
   const [ reviews, setReviews] = useState("");
   const [ searchInput, setSearchInput ] = useState("");
+  // const [ filteredReviews, setFilteredReviews ] = useState("");
+
     const fetchReviews = async () => {
       let url = `${API_BASE_URL}/reviews/${localStorage.user}`;
       const response = await fetch(url);
@@ -27,39 +30,40 @@ export const MyReviews = (props) => {
     let rating;
     let review;
     let driverComment;
+    let filteredReviews;
     if (reviews) {
-      let filteredReviews = reviews.filter(review => review.plateNumber.includes(searchInput));
-      review = filteredReviews.map((review, index) => { 
-        if (review.isPostive === 'true') {
-          rating = <Icon>thumb_up</Icon>
-        } else {
-          rating = <Icon>thumb_down</Icon>
-        }
+      filteredReviews = reviews.filter(review => review.plateNumber.includes(searchInput.toUpperCase()));
+      // review = filteredReviews.map((review, index) => { 
+      //   if (review.isPostive === 'true') {
+      //     rating = <Icon>thumb_up</Icon>
+      //   } else {
+      //     rating = <Icon>thumb_down</Icon>
+      //   }
 
-        if (review.comment) {
-          driverComment = <p> Driver Response: {review.comment}</p>
-        } 
+      //   if (review.comment) {
+      //     driverComment = <p> Driver Response: {review.comment}</p>
+      //   } 
 
-        return (
-          <li className='review-item' key={review._id} tabIndex='0'>
-            <article className='review-header'>
-              <article className='review-title'>
-                <img className='isClaimed-icon' src='https://cdn4.iconfinder.com/data/icons/flatastic-11-1/256/user-green-512.png' alt='green user icon'></img>
-                  {review.plateNumber} {review.plateState}         
-                {/* <p id='review-time'>{today}</p> */}
-              </article>
-              <article className='review-rating'>
-                <p className='rating'>{rating}</p>
-              </article>
-            </article>
-            {/* <h1 className='plate-number'>{review.plateNumber}</h1><br/> */}
-            {/* <img className='review-img' src='https://i.pinimg.com/236x/29/55/38/295538a452d701c9189d0fa8f5b36938--white-truck-bad-parking.jpg' alt='review'></img> */}          
-            {/* Do we want to add information about how long ago this was posted, i.e. 2m or 2h */}          
-            <p className='message'>Review: {review.message}</p>
-            <p>{driverComment}</p>
-          </li>
-        )
-      })
+      //   return (
+      //     <li className='review-item' key={review._id} tabIndex='0'>
+      //       <article className='review-header'>
+      //         <article className='review-title'>
+      //           <img className='isClaimed-icon' src='https://cdn4.iconfinder.com/data/icons/flatastic-11-1/256/user-green-512.png' alt='green user icon'></img>
+      //             {review.plateNumber} {review.plateState}         
+      //           {/* <p id='review-time'>{today}</p> */}
+      //         </article>
+      //         <article className='review-rating'>
+      //           <p className='rating'>{rating}</p>
+      //         </article>
+      //       </article>
+      //       {/* <h1 className='plate-number'>{review.plateNumber}</h1><br/> */}
+      //       {/* <img className='review-img' src='https://i.pinimg.com/236x/29/55/38/295538a452d701c9189d0fa8f5b36938--white-truck-bad-parking.jpg' alt='review'></img> */}          
+      //       {/* Do we want to add information about how long ago this was posted, i.e. 2m or 2h */}          
+      //       <p className='message'>Review: {review.message}</p>
+      //       <p>{driverComment}</p>
+      //     </li>
+      //   )
+      // })
     };
 
   return (
@@ -98,7 +102,7 @@ export const MyReviews = (props) => {
           </fieldset>
           </div>
         <ul className='reviews'>
-          {review}
+          <Review reviews={filteredReviews}/>
         </ul>
       </section>
     </div>
--- src/components/plate.js
+++ src/components/plate.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect }  from 'react'; 
 import {API_BASE_URL} from '../config';
+import Review from './Review';
 import { Icon } from 'react-materialize';
 import { Link } from 'react-router-dom';
 
@@ -32,40 +33,40 @@ export const Plate = (props) => {
     let review;
     let driverComment;
 
-    if (reviews) {
-      review = reviews.map((review, index) => { 
-        if (review.isPositive === 'true') {
-          rating = <Icon>thumb_up</Icon>
-        } else {
-          rating = <Icon>thumb_down</Icon>
-        }
+    // if (reviews) {
+    //   review = reviews.map((review, index) => { 
+    //     if (review.isPositive === 'true') {
+    //       rating = <Icon>thumb_up</Icon>
+    //     } else {
+    //       rating = <Icon>thumb_down</Icon>
+    //     }
 
-        if (review.comment) {
-          driverComment = <p> Driver Response: {review.comment}</p>
-        } 
+    //     if (review.comment) {
+    //       driverComment = <p> Driver Response: {review.comment}</p>
+    //     } 
 
-        return (
-          <li className='review-item' key={review._id} tabIndex='0'>
-            <article className='review-header'>
-              <article className='review-title'>
-                <img className='isClaimed-icon' src='https://cdn4.iconfinder.com/data/icons/flatastic-11-1/256/user-green-512.png' alt='green user icon'></img>
-                  {review.plateNumber} {review.plateState}         
-                {/* <p id='review-time'>{today}</p> */}
-              </article>
+    //     return (
+    //       <li className='review-item' key={review._id} tabIndex='0'>
+    //         <article className='review-header'>
+    //           <article className='review-title'>
+    //             <img className='isClaimed-icon' src='https://cdn4.iconfinder.com/data/icons/flatastic-11-1/256/user-green-512.png' alt='green user icon'></img>
+    //               {review.plateNumber} {review.plateState}         
+    //             {/* <p id='review-time'>{today}</p> */}
+    //           </article>
               
-              <article className='review-rating'>
-                <p className='rating'>{rating}</p>
-              </article>
-            </article>
-            {/* <h1 className='plate-number'>{review.plateNumber}</h1><br/> */}
-            {/* <img className='review-img' src='https://i.pinimg.com/236x/29/55/38/295538a452d701c9189d0fa8f5b36938--white-truck-bad-parking.jpg' alt='review'></img> */}            
-            {/* Do we want to add information about how long ago this was posted, i.e. 2m or 2h */}          
-            <p className='message'>Review: {review.message}</p>
-            <p>{driverComment}</p>
-          </li>
-        )
-      })
-    };
+    //           <article className='review-rating'>
+    //             <p className='rating'>{rating}</p>
+    //           </article>
+    //         </article>
+    //         {/* <h1 className='plate-number'>{review.plateNumber}</h1><br/> */}
+    //         {/* <img className='review-img' src='https://i.pinimg.com/236x/29/55/38/295538a452d701c9189d0fa8f5b36938--white-truck-bad-parking.jpg' alt='review'></img> */}            
+    //         {/* Do we want to add information about how long ago this was posted, i.e. 2m or 2h */}          
+    //         <p className='message'>Review: {review.message}</p>
+    //         <p>{driverComment}</p>
+    //       </li>
+    //     )
+    //   })
+    // };
   
 
   return (
@@ -80,7 +81,7 @@ export const Plate = (props) => {
         <p className="karma-score">Karma Score: {plate.karma}</p>
       </div>
       <ul className='reviews'>
-        {review}
+        <Review reviews={reviews} />
       </ul>
     </div>
 
