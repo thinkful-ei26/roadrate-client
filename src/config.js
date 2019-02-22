bbae86ad52f70e7ec86d8fef1010f641127b2e8d
Jordan <jordanhaddadi@gmail.com>
2019-02-22 17:29:21 -0500

    added api, because TJ----yeah....

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   // TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-  API_BASE_URL: process.env.API_URL || 'https://road-rate-server.herokuapp.com/'
+  API_BASE_URL: process.env.API_URL || 'https://road-rate-server.herokuapp.com/api'
 };
