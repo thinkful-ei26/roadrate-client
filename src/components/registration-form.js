cffcba2e866ec9deea261742cfd685ec1b48963a
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-24 07:18:17 -0500

    fixed regex on login & registration forms

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -184,7 +184,7 @@ export const RegistrationForm = () => {
           type="password"
           name="password"
           required
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$" 
           title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
         />
         </label>
