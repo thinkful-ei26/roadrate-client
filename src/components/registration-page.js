3c83c822810d9d72e9a35433143e4a845a2cd228
Jordan <jordanhaddadi@gmail.com>
2019-03-01 10:48:50 -0500

    added confidential h5 to reg form

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,6 +12,9 @@ export function RegistrationPage() {
                 className="registration-icon"
             />
             <h1>Register to RoadRate</h1>
+            <div className='reg-h5'>
+                <h5 id="reg-h5">Information is confidential and 100% anonymous</h5>
+            </div>
             <RegistrationForm />
         </div>
     );
