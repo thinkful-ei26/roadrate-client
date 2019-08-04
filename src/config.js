e2bed0f589b7b701c937b5cd58377692f8984a19
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-08-04 12:40:47 -0400

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/roadrate?retryWrites=true&w=majority', // local
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/test?retryWrites=true&w=majority', // production
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/roadrate?retryWrites=true&w=majority', // production
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/test?retryWrites=true&w=majority', // development
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   API_BASE_URL: process.env.API_URL || 'https://road-rate-server.herokuapp.com/api',
