ffd8e74c2eb246c351842c37ba7cefd37c5bd064
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-08-04 11:42:32 -0400

    fixed the database

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://jordan:haddadi@roadrate-shard-00-00-qquzy.mongodb.net:27017,roadrate-shard-00-01-qquzy.mongodb.net:27017,roadrate-shard-00-02-qquzy.mongodb.net:27017/roadrate?ssl=true&replicaSet=RoadRate-shard-0&authSource=admin&retryWrites=true',
-  // TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/roadrate?retryWrites=true&w=majority', // local
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jordan:haddadi@roadrate-qquzy.mongodb.net/test?retryWrites=true&w=majority', // production
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   API_BASE_URL: process.env.API_URL || 'https://road-rate-server.herokuapp.com/api',
